dc52e80526e87ab03f9f5bd56fb4c3b0d7449484
Fabricio assuncao <fabricioassuncao855@outlook.com>
2022-08-02 19:13:37 -0300

    fiz o post

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from multiprocessing import context
 from turtle import pos
 from django.shortcuts import render, get_object_or_404
-import requests 
+import requests
+import  json
 from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Postagem
 from django.forms.models import model_to_dict
 
@@ -17,19 +21,21 @@ def listar_postagem(request):
     for postagem in postagens:
         c.append({
             'id': postagem.id,
-            'autor': postagem.autor.username
+            'autor': postagem.autor.username,
+            'url': postagem.url,
+            'descricao': postagem.url
         })
     context = {'postagens': c}
     return JsonResponse(context)
-
+@csrf_exempt
 def adicionar_postagem(request):
     if request.method == 'POST':
-        valores = request.content_params
-        Postagem.objects.create(valores)
+        valores = request.body
+        post = json.loads(valores.decode('utf-8'))
+        Postagem.objects.create(autor_id=1, descricao=post.get('descricao'), url=post.get('url'))
         return JsonResponse({'message': 'sucesso',
-        'status_code':200})
+                             'code': 200})
 
-        pass
 
 def alterar_postagem(request, id_post):
     if request.method == 'PUT':
